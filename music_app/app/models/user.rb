c7345eea5ac8004c143a0bc9cd15c8b058a34436
Michael Gastello <michaelgastello@aol.com>
2023-02-21 14:15:06 -0500

    added auth

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6 } allow_nil: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        bcrypt_obj = BCrypt::Password.new(self.password_digest)
+        bcrypt_obj.is_password?(password)
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
